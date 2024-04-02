362408ac0a04faee97b8cdee332c89e38e2c16a4
Aditya <aditya.11918716@lpu.in>
2024-04-02 16:44:08 +0530

    tailwind plugin fixed

--- src/lib/components/IconCard.svelte
+++ src/lib/components/IconCard.svelte
@@ -1,5 +1,5 @@
 <article>
     <img src="https://static.wixstatic.com/media/454d4b_d8eec91f91d04b99b3bb02ca3b51b14f~mv2.png/v1/fill/w_142,h_142,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/Layer%201702.png" alt="ICOn">
     <h3>Effortless Organization</h3>
-    <p>Easily trace equipment locations, ensuring no movement goes unsupervised. </p>
+    <p class="">Easily trace equipment locations, ensuring no movement goes unsupervised. </p>
 </article>
