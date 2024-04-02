06565ac50e644400b70e105cb688bea3ca5b86bc
Aditya <aditya.11918716@lpu.in>
2024-04-02 16:37:11 +0530

    added tailwind css plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import IconCard from "$lib/components/IconCard.svelte";
+
+ 
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<IconCard />
