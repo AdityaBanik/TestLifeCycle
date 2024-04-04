a90587737bf133bdcacde0348c9c411927c102f4
Abhishek Patil <138243988+Abhi9765@users.noreply.github.com>
2024-04-04 07:38:25 -0700

    layout created for mobile screen

--- src/routes/functional-test-area/+page.svelte
+++ src/routes/functional-test-area/+page.svelte
@@ -1,3 +1,58 @@
 <script lang="ts">
-
+	let title = 'Passive Safety (Crash)';
+	let subtitle = 'No Margin For Error';
+	let src =
+		'https://static.wixstatic.com/media/454d4b_f78241a2c7e3436c839327796491aaa6~mv2.jpeg/v1/fill/w_848,h_516,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/powertrain-scaled-1-1920x1080.jpeg';
 </script>
+
+<div class="container p-0">
+	<div class="headerTemplate bg-blue-50 flex justify-around">
+	
+
+        <div class="template flex flex-col justify-end gap-1 bg-cover bg-no-repeat bg-center">
+         <h3 class="text-sm font-bold">{title}</h3>
+         <h5 class="text-xs font-bold">{subtitle}</h5>
+        </div>
+		
+        <div class="info translate-y-14 w-6/12 px-2">
+			<img {src} alt="car img" />
+		</div>
+
+	</div>
+
+	<div class="discription p-4 flex">
+		<div class="detailDiscription mt-20">
+			<p class="text-justify text-xs">
+				It is a long established fact that a reader will be distracted by the readable content of a
+				page when looking at its layout. The point of using Lorem Ipsum is that it has a
+				more-or-less normal distribution of letters, as opposed to using 'Content here, content
+				here', making it look like readable English. Many desktop publishing packages and web page
+				editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+				uncover many web sites still in their infancy. Various versions have evolved over the years,
+				sometimes by accident, sometimes on purpose (injected humour and the like). It is a long
+				established fact that a reader will be distracted by the readable content of a page when
+				looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+				distribution of letters, as opposed to using 'Content here, content here', making it look
+				like readable English. Many desktop publishing packages and web page editors now use Lorem
+				Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
+				sites still in their infancy. Various versions have evolved over the years, sometimes by
+				accident, sometimes on purpose (injected humour and the like).
+			</p>
+			<br />
+
+			<p class="text-justify text-xs">
+				It is a long established fact that a reader will be distracted by the readable content of a
+				page when looking at its layout. The point of using Lorem Ipsum is that it has a
+				more-or-less normal distribution of letters, as opposed to using 'Content here, content
+				here', making it look like readable English.
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+.template{
+    background-image: url('https://static.wixstatic.com/media/454d4b_9cc8ee97f786412c8fb983ff9f28bb63~mv2.png/v1/fill/w_944,h_464,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/454d4b_9cc8ee97f786412c8fb983ff9f28bb63~mv2.png');
+}
+
+</style>
