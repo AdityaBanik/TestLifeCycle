33cbc3ad5536739ce126d048084675f6cff944cc
Abhishek Patil <138243988+Abhi9765@users.noreply.github.com>
2024-04-12 03:06:49 -0700

    Header template added for home component

--- src/lib/components/shared/CTC.svelte
+++ src/lib/components/shared/CTC.svelte
@@ -2,17 +2,18 @@
 	import Button from '$lib/components/buttons/Button.svelte';
 	export let btnText: string;
 	export let title: string;
-    export let description = '';
+	export let description = '';
 	export let BtnClass = '';
-    
 </script>
 
 <section class="text-center lg:py-10 p-3 md:px-14 md:py-10">
 	<div class="container flex flex-col items-center gap-7 md:gap-7">
 		<div>
-			<h3 class="text-blue-900 leading-normal">
+			<div class="w-full">
+			<h3 class="text-blue-900 leading-normal lg:text-9xl">
 				{title}
 			</h3>
+		</div>
 			<p class="text-blue-900">{description}</p>
 			<h4 class="text-blue-900">Your way!</h4>
 		</div>
@@ -23,8 +24,9 @@
 
 <style>
 	h3 {
-		font-size: clamp(1.5rem, 4vw, 5rem) !important;
-		font-family: 'Bebas Neue', sans-serif;
+		font-size: clamp(1.5rem, 4vw, 12rem) !important;
+		/* font-family: 'Bebas Neue', sans-serif; */
+		font-weight: bolder;
 		font-weight: 200;
 		font-style: normal;
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,25 @@
 	import AccordianItem from '$lib/components/accordian/AccordianItem.svelte';
 	import TitanHome from '$lib/components/animation/TitanHome.svelte';
 	import IconCard from '$lib/components/cards/IconCard.svelte';
+	import Button from '$lib/components/buttons/Button.svelte';
+	export let btnText: string = 'Learn more';
 </script>
 
+<section class="text-center lg:py-10 p-3 md:px-14 md:py-10">
+	<div class="container flex flex-col items-center gap-4 md:gap-7">
+		<div>
+	
+				<h1 class=" leading-normal text-lg   font-bold">
+					Test Lifecycle Simplified
+				</h1>
+	
+			<h2 class=" font-bold">The adaptable platform that lets you validate products,<strong>your way!</strong></h2>
+		</div>
 
-<section class=" relative  z-20 bg-white ">
+		<Button text={btnText} />
+	</div>
+</section>
+<section class=" relative z-20 bg-white">
 	<div class="container pt-10 md:pt-20">
 		<h2 class="font-bold line-height md:text-2xl lg:text-4xl mb-2 md:mb-4 max-w-sm md:max-w-xl">
 			Where Gear Management Meets Effortless Excellence.
@@ -20,3 +35,13 @@
 
 <TitanHome />
 
+<style>
+
+
+.fluid-title{
+
+}
+h1 {
+   font-size: clamp(0.6rem,2.5vw,2rem) !important;
+}
+</style>
