9709ee6dc5c2396eeb95d485048bfeaf3fc990df
PolSnehal <Snehal.pol@12thwonder.com>
2024-04-12 17:55:27 +0530

    Created Resource center page nad home Bottom digital transformation section

---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,94 @@
+<script>
+    import icon1 from '$lib/assets/Home/icon 1.png';
+    import icon2 from '$lib/assets/Home/icon 2.png';
+    import icon3 from '$lib/assets/Home/icon 3.png';
+    import icon4 from '$lib/assets/Home/icon 4.png';
+    import icon5 from '$lib/assets/Home/icon 5.png';
+    import icon6 from '$lib/assets/Home/icon 6.png';
+    import img1 from '$lib/assets/Home/pc.png';
+</script>
+
+<section class="px-4 py-8 flex items-center flex-wrap ml-10">
+    <div class="w-full md:w-1/2 lg:w-1/2 mb-6">
+        <h2 class=" border-color1 text-3xl text-gray-600 border-l-4 pl-5 font-bold mb-2"><span class="color1">TITAN</span> is accelerating digital transformation</h2>
+        <p class="text-gray-600 font-bold pt-3 pl-6">It's all about creating engaging experiences, fostering collaboration, and building a happier workplace</p>
+        
+        <img src={img1} alt="img1" class="w-full md:hidden lg:hidden mb-6">
+
+        <div class="grid grid-cols-3  md:grid-cols-2  gap-4 pt-5">
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon1} alt="Icon">
+                <p class="Description text-black md:-ml-5 ">
+                    <span class=" pcolor font-bold text-3xl mb-4">25%</span><br>Turnaround time reduced.
+            
+                </p>
+            </article>
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon2} alt="Icon">
+                <p class=" Description text-black md:-ml-5">
+                    <span class="pcolor font-bold text-2xl">70%</span> <br>Increase in collaboration productivity
+                </p>
+            </article>
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon6} alt="Icon">
+                <p class="Description text-black md:-ml-5">
+                    <span class="pcolor font-bold text-3xl">50%</span> <br>Time saved to search & reuse information
+                </p>
+            </article>
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon4} alt="Icon">
+                <p class="Description text-black md:-ml-5">
+                    <span class="pcolor font-bold text-3xl">30%</span> <br>Decrease in quality issues
+                </p>
+            </article>
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon5} alt="Icon">
+                <p class="Description text-black md:-ml-5">
+                    <span class="pcolor font-bold text-3xl">20%</span> <br>Increase in operational efficiency
+                </p>
+            </article>
+            <article class="mb-4 flex items-center">
+                <img class="iconimg mr-1" src={icon3} alt="Icon">
+                <p class="Description text-black md:-ml-5">
+                    <span class=" pcolor font-bold text-3xl">40%</span> <br> Reduction in error rate
+                </p>
+            </article>
+        </div>
+    </div>
+
+    
+        <img src={img1} alt="dimg" class="w-700 h-400 hidden md:block lg:block w-full mt-10 md:w-1/2 lg:w-1/2 justify-end">
+
+</section>
+
+<style>
+    .iconimg{
+        width: 90px;
+        height: 90px;
+    }
+    .pcolor{
+        color: #fa854f;
+        font-size: clamp(0.5rem, 3vw, 1.5rem);
+    }
+    .color1{
+        color: #fa854f; 
+    }
+    .border-color1{
+        border-left-color: #fa854f;
+    }
+    .Description{
+        font-size: clamp(0.4rem, 2vw, 1.0rem);
+    }
+   
+    @media only screen and (max-width: 600px)  {
+        .iconimg {
+            width: 50px; 
+            height: 50px;
+        }
+        .Description{
+            margin-left: -4;
+        }
+       
+    }
+   
+</style>
---
+++ src/routes/resource-center/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  import about from '$lib/assets/Resource-center.png';
+</script>
+
+<style>
+  .title {
+        font-size: clamp(0.2rem, 4.5vw, 2rem);
+  }
+
+  .btitle {
+      font-size: clamp(0.2rem, 4vw, 1.5rem);
+    }
+
+    .bparagraph {
+      font-size: clamp(0.2rem, 3vw, 0.9rem);
+    }
+</style>
+
+<div class="text-3xl ml-10 font-bold mb-16 mt-10 title">
+  <h1>Resource Center</h1>
+</div>
+
+<div class="flex flex-col md:flex-row p-10 mb-10 ml-10 mr-10">
+  <!-- box 1 -->
+  <div class="flex-item flex-1 md:max-h-500px md:w-1/2">
+    <div class="flex justify-center items-center h-full">
+      <img src={about} alt="Image Description">
+    </div>
+  </div>
+  <!-- box 2 -->
+  <div class="flex-item p-4 flex-1 bg-white md:mt-10 md:mb-10 md:-ml-40 md:relative shadow-lg md:w-1/2">
+    <h2 class="text-lg mb-2 ml-5 text-blue-600 mt-5 btitle ">Case Study</h2>
+    <h3 class="text-md ml-5 text-black font-bold mb-2 ">Transforming Test Lifecycle Management: Real-World Success Stories with Titan</h3>
+    <p class="text-black  ml-5 mb-4 bparagraph">Facing the automotive industry's need for speed and safety, the Titan Test Lifecycle Management Solution automates and streamlines test processes, enabling teams to innovate efficiently. By integrating planning, execution, analysis, and reporting, Titan significantly boosts efficiency and accuracy. This case study showcases Titan's impact in a crash test scenario, highlighting its role in enhancing quality and accelerating the introduction of new automotive technologies.</p>
+    <div> 
+      <button class="bg-black ml-5 mt-2 hover:bg-gray-700 text-white py-2 px-16 rounded-3xl">Download</button>
+    </div>
+  </div>
+</div>
