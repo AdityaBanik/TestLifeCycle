b0d3043dad7311bf53123c09d46fac98f7046b0d
Abhishek Patil <138243988+Abhi9765@users.noreply.github.com>
2024-04-17 00:56:36 -0700

    updated code for homepage

--- src/routes/[[lang=locale]]/+page.svelte
+++ src/routes/[[lang=locale]]/+page.svelte
@@ -2,15 +2,14 @@
 	import { fade, fly } from 'svelte/transition';
 	import { backOut } from 'svelte/easing';
 	import { onMount } from 'svelte';
-
 	import TitanHome from '$lib/components/animation/TitanHome.svelte';
 	import Button from '$lib/components/buttons/Button.svelte';
 	import Cta from '$lib/components/shared/CTA.svelte';
-
 	import monitorImage from '$lib/assets/Home/pc.png';
 	import HomeFeatures from '$lib/components/sections/HomeFeatures.svelte';
 	import type { PageData } from './$types';
-
+	export let data: PageData;
+	export let btnText: string = 'Learn more';
 	let animation = false;
 
 	onMount(() => {
@@ -19,7 +18,31 @@
 		}
 	});
 
-	export let data: PageData;
+	let currentSlide = 0;
+	let previousSlide = 0;
+	let direction = 'next';
+
+	const slides = [
+		'src/lib/assets/automated reports 2.png',
+		'src/lib/assets/project insights 2.png',
+		'src/lib/assets/data mgmt 2.png',
+		'src/lib/assets/incident mgmt 2.png',
+		'src/lib/assets/kpi 2.png'
+	];
+
+	const buttons = [
+		'Automated Reports',
+		'Project Insights',
+		'Data Management',
+		'Incident Management',
+		'KPI Dashboard'
+	];
+
+	function changeSlide(index: any) {
+		previousSlide = currentSlide;
+		currentSlide = index;
+		direction = index > previousSlide ? 'next' : 'prev';
+	}
 </script>
 
 {#key animation}
@@ -63,6 +86,57 @@
 	{#each data.page?.Features || [] as item, i}
 		<HomeFeatures title={item?.title || ''} items={item?.features || []} alignRight={i == 1} />
 	{/each}
+</section>
+
+<section class="bg-gradient-to-b from-indigo-500 to-purple-700 prose">
+	<div class="container py-6 flex flex-col items-center">
+		<div class="submainone flex flex-col">
+			<div class="title&bullets flex flex-col gap-4">
+				<div class="title">
+					<h3 class="font-bold text-sm">
+						<span class="text-yellow-500 font-bold"
+							>One platform to monitor your Test Lifecycle Insights
+						</span><span class="font-bold text-white"
+							>in real-time, analyze data, generate value-added reports, and more.</span
+						>
+					</h3>
+				</div>
+				<div class="bullets px-4">
+					<h4 class="font-bold text-white ml-3">{buttons[currentSlide]}</h4>
+					<ul>
+						{#each ['Use multiple report templates', 'Review and Approved/Reject using Titan', 'Store & release in the system'] as bullet}
+							<li class="text-white p-0 m-0">{bullet}</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+
+			<div class="slide-container">
+				<div class="slide {direction}">
+					{#each slides as slide, index}
+						<img
+							class={direction === 'next' ? 'slide-in-up' : 'slide-in-down'}
+							src={slide}
+							alt={'Slide ' + (index + 1)}
+							style="display: {index === currentSlide
+								? 'block'
+								: 'none'}; animation-delay: {index === currentSlide ? '0s' : '0.2s'};"
+						/>
+					{/each}
+				</div>
+			</div>
+		</div>
+
+		<div class="allBtns flex flex-col gap-4 w-[50%] text-center">
+			{#each buttons as button, index}
+				<button
+					on:click={() => changeSlide(index)}
+					class="bg-cyan-500 no-underline py-2.5 fluid-font px-4 lg:px-10 lg:py-2 font-medium text-white rounded-full hover:bg-blue-700 active:bg-gray-400"
+					>{button}</button
+				>
+			{/each}
+		</div>
+	</div>
 </section>
 
 <Cta
@@ -115,5 +189,37 @@
 	}
 	.fluid-subtitle {
 		font-size: clamp(0.65rem, 1.8vw, 2rem) !important;
+	}
+
+	.slide {
+		display: flex;
+		position: relative;
+		overflow: hidden;
+	}
+
+	.slide-in-up {
+		animation: slide-in-up 0.5s ease forwards;
+	}
+
+	.slide-in-down {
+		animation: slide-in-down 0.5s ease forwards;
+	}
+
+	@keyframes slide-in-up {
+		from {
+			transform: translateX(100%);
+		}
+		to {
+			transform: translateX(0);
+		}
+	}
+
+	@keyframes slide-in-down {
+		from {
+			transform: translateX(-100%);
+		}
+		to {
+			transform: translateX(0);
+		}
 	}
 </style>
