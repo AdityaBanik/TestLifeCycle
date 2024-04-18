2bb1e30690426d4375ceee76b39b0a9d03f09d6d
Aditya <aditya.11918716@lpu.in>
2024-04-18 21:33:25 +0530

    cloudflare support added

--- src/routes/[[lang=locale]]/why-choose-us/+page.svelte
+++ src/routes/[[lang=locale]]/why-choose-us/+page.svelte
@@ -15,7 +15,7 @@
 		<img src={bgimage} alt="Image1" class="w-64 h-64 mx-auto mb-2 rounded-full" />
 		<div class="lg:w-[50%] sm:p-5  mx-auto">
 			<h2 class="text-lg text-white mb-10 pr-10 fluid-subtitle ">
-				{data.page?.highlights.title}
+				{data.page?.highlights?.title}
 			</h2>
 		</div>
 	</section>
@@ -25,8 +25,7 @@
 		<div
 		class="mt-10 md:mt-28 grid grid-cols-2  gap-4 md:gap-6 xl:gap-y-14"
 	>
-	
-		{#each data.page?.highlights.cards || [] as card}
+		{#each data.page?.highlights?.cards|| [] as card}
 			<IconCard
 				title={card?.title || ''}
 				description={card?.description || ''}
